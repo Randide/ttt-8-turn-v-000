b37795cf43d8d6535da7d109eb9cd364214ea9ff
Randi Nash <randidena@live.com>
2017-09-23 21:50:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def input_to_index(letsgo)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
     true
   else
     false
