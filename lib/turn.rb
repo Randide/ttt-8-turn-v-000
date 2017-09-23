aa476910b7bb327f3d764df3888bde12d1ee6950
Randi Nash <randidena@live.com>
2017-09-23 21:52:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
     true
   else
     false
