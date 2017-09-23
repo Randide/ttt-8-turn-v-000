0f6f8f81571a36b71961df8fc84113c7c26f451a
Randi Nash <randidena@live.com>
2017-09-23 21:35:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,14 @@
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
 def valid_move?(board, index)
   if index.between?(0,8)
     if position_taken?(board, index) == false
@@ -9,7 +20,6 @@ def valid_move?(board, index)
     return false
   end
 end
-#code your #valid_move? method here
 
 
 def position_taken?(board, index)
